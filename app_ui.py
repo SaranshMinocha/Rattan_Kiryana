fde5c958ddc5b1e1378a996d995349ec207e59ec
SaranshMinocha <saranshminocha9@gmail.com>
2026-09-13 17:05:55 +0530

    chore: point ui to live cloud backend

--- app_ui.py
+++ app_ui.py
@@ -3,7 +3,7 @@ import requests
 from datetime import datetime
 import os
 
-API_URL = os.getenv("API_URL","http://backend:8000")
+API_URL = os.getenv("API_URL","https://shelfsense-backend-cg19.onrender.com")
 
 st.set_page_config(
     page_title="ShelfSense",
